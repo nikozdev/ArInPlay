5b180b0f7dc02f0dd35fd4269dc0afd501cde9ba
nikozdev <nikozdev@nikozdev.net>
2023-07-24 19:39:13 -0700

    !lib!test!build!system!cmake!std!fmt!header
    [CMakeLists.txt]
    > adjusted the tHello test regular expression
    > imported and linked fmt library because std::format is not complete on clang
    > > i am using macbook with arm-chip and clang tools by the way
    [Main.cxx]
    > included a bunch of standard headers
    > included fmt/format.h header
    > testing fmt with hello world and exception

--- Main.cxx
+++ Main.cxx
@@ -1,8 +1,30 @@
 //headers
+#include <fmt/format.h>
+//-//standard
 #include <cstdio>
+#include <exception>
+//-//-//numbers
+#include <cmath>
+#include <limits>
+#include <numeric>
+//-//-//memory
+#include <memory>
+//-//-//strings
+#include <string>
+#include <string_view>
+//-//-//input-output
+#include <fstream>
+#include <iostream>
 //actions
 int main(int vArgC, char *vArgV[])
 {
-    std::fprintf(stdout, "HelloWorld\n");
-    return 0;
+	try
+	{
+        throw std::runtime_error("HelloError");
+	}
+	catch(const std::exception &rError)
+	{
+		fmt::println("we have got an exception: {0}", rError.what());
+	}//catch(std::exception&)
+	return 0;
 }//main
